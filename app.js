b3bb9be342bffa2a6c95e5f75300ab06355bcef7
Guillermo Vaz <guillermovaz@Guillermos-MBP.lan>
2024-09-26 18:22:35 -0700

    Added some stuff

---
+++ app.js
@@ -0,0 +1,21 @@
+git command LINE
+
+git init - plug memory card 
+
+git add . save all the changes made 
+(can also use git and document file to save one specific thing )
+gid_t commit -m   (-m describes the changes)
+
+
+
+git log (log of all your save changes)
+
+git checkout (plug in the encrypted key)
+
+connect to git hub
+
+git push push the saved progress to the internet or website
+
+git checkout -b new-branch (trail off and create your own thing with pre exisitng files on a new branch to not change og stuff)
+
+git pull orgin master (pull the stuff from the stuff on github the updates)
